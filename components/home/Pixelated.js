c64526953a022050c1f9f0d21630ca4169e2ec80
stravo1@msi <stravoone@gmail.com>
2022-08-19 19:23:03 +0530

    change: reduced font sizes for pixelated texts

--- components/home/Pixelated.js
+++ components/home/Pixelated.js
@@ -1,6 +1,6 @@
 const Pixelated = ({ children }) => {
   return (
-    <div className="flex items-end break-words md:max-h-[45vw] overflow-hidden py-6 md:ml-4 basis-1/2 text-[50px] md:text-[60px] lg:text-[80px] xl:text-[100px] font-cursive lg:pl-8">
+    <div className="flex items-end break-words md:max-h-[45vw] overflow-hidden py-6 md:ml-4 basis-1/2 text-[40px] md:text-[50px] lg:text-[75px] xl:text-[90px] font-cursive lg:pl-8">
       {children}
     </div>
   );
